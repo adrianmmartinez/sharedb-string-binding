1d5d419cda7165dc55b8d1a98967c8838167feba
Adrian Martinez <adrianmartinez@MacBookAir.local>
2019-01-21 16:48:02 -0800

    sharedb-codemirror

--- index.js
+++ index.js
@@ -1,109 +1,110 @@
 var TextDiffBinding = require('text-diff-binding');
 
-module.exports = StringBinding;
+module.exports = ShareDBCodeMirror;
 
-function StringBinding(element, doc, path) {
-  TextDiffBinding.call(this, element);
-  this.doc = doc;
-  this.path = path || [];
-  this._opListener = null;
-  this._inputListener = null;
+function ShareDBCodeMirror(codemirror, doc, path) {
+    TextDiffBinding.call(this, codemirror.getInputField());
+    this.doc = doc;
+    this.path = path || [];
+    this._opListener = null;
+    this._inputListener = null;
 }
-StringBinding.prototype = Object.create(TextDiffBinding.prototype);
-StringBinding.prototype.constructor = StringBinding;
 
-StringBinding.prototype.setup = function() {
-  this.update();
-  this.attachDoc();
-  this.attachElement();
+ShareDBCodeMirror.prototype = Object.create(TextDiffBinding.prototype);
+ShareDBCodeMirror.prototype.constructor = ShareDBCodeMirror;
+
+ShareDBCodeMirror.prototype.setup = function() {
+    this.update();
+    this.attachDoc();
+    this.attachElement();
 };
 
-StringBinding.prototype.destroy = function() {
-  this.detachElement();
-  this.detachDoc();
+ShareDBCodeMirror.prototype.destroy = function() {
+    this.detachElement();
+    this.detachDoc();
 };
 
-StringBinding.prototype.attachElement = function() {
-  var binding = this;
-  this._inputListener = function() {
-    binding.onInput();
-  };
-  this.element.addEventListener('input', this._inputListener, false);
+ShareDBCodeMirror.prototype.attachElement = function() {
+    var binding = this;
+    this._inputListener = function() {
+        binding.onInput();
+    };
+    this.codeMirror.addEventListener('input', this._inputListener, false);
 };
 
-StringBinding.prototype.detachElement = function() {
-  this.element.removeEventListener('input', this._inputListener, false);
+ShareDBCodeMirror.prototype.detachElement = function() {
+    this.codeMirror.removeEventListener('input', this._inputListener, false);
 };
 
-StringBinding.prototype.attachDoc = function() {
-  var binding = this;
-  this._opListener = function(op, source) {
-    binding._onOp(op, source);
-  };
-  this.doc.on('op', this._opListener);
+ShareDBCodeMirror.prototype.attachDoc = function() {
+    var binding = this;
+    this._opListener = function(op, source) {
+        binding._onOp(op, source);
+    };
+    this.doc.on('op', this._opListener);
 };
 
-StringBinding.prototype.detachDoc = function() {
-  this.doc.removeListener('op', this._opListener);
+ShareDBCodeMirror.prototype.detachDoc = function() {
+    this.doc.removeListener('op', this._opListener);
 };
 
-StringBinding.prototype._onOp = function(op, source) {
-  if (source === this) return;
-  if (op.length === 0) return;
-  if (op.length > 1) {
-    throw new Error('Op with multiple components emitted');
-  }
-  var component = op[0];
-  if (isSubpath(this.path, component.p)) {
-    this._parseInsertOp(component);
-    this._parseRemoveOp(component);
-  } else if (isSubpath(component.p, this.path)) {
-    this._parseParentOp();
-  }
+ShareDBCodeMirror.prototype._onOp = function(op, source) {
+    if (source === this) return;
+    if (op.length === 0) return;
+    if (op.length > 1) {
+        throw new Error('Op with multiple components emitted');
+    }
+    var component = op[0];
+    if (isSubpath(this.path, component.p)) {
+        this._parseInsertOp(component);
+        this._parseRemoveOp(component);
+    } else if (isSubpath(component.p, this.path)) {
+        this._parseParentOp();
+    }
 };
 
-StringBinding.prototype._parseInsertOp = function(component) {
-  if (!component.si) return;
-  var index = component.p[component.p.length - 1];
-  var length = component.si.length;
-  this.onInsert(index, length);
+ShareDBCodeMirror.prototype._parseInsertOp = function(component) {
+    if (!component.si) return;
+    var index = component.p[component.p.length - 1];
+    var length = component.si.length;
+    this.onInsert(index, length);
 };
 
-StringBinding.prototype._parseRemoveOp = function(component) {
-  if (!component.sd) return;
-  var index = component.p[component.p.length - 1];
-  var length = component.sd.length;
-  this.onRemove(index, length);
+ShareDBCodeMirror.prototype._parseRemoveOp = function(component) {
+    if (!component.sd) return;
+    var index = component.p[component.p.length - 1];
+    var length = component.sd.length;
+    this.onRemove(index, length);
 };
 
-StringBinding.prototype._parseParentOp = function() {
-  this.update();
+ShareDBCodeMirror.prototype._parseParentOp = function() {
+    this.update();
 };
 
-StringBinding.prototype._get = function() {
-  var value = this.doc.data;
-  for (var i = 0; i < this.path.length; i++) {
-    var segment = this.path[i];
-    value = value[segment];
-  }
-  return value;
+ShareDBCodeMirror.prototype._get = function() {
+    var value = this.doc.data;
+    for (var i = 0; i < this.path.length; i++) {
+        var segment = this.path[i];
+        value = value[segment];
+    }
+    return value;
 };
 
-StringBinding.prototype._insert = function(index, text) {
-  var path = this.path.concat(index);
-  var op = {p: path, si: text};
-  this.doc.submitOp(op, {source: this});
+ShareDBCodeMirror.prototype._insert = function(index, text) {
+    var path = this.path.concat(index);
+    var op = {p: path, si: text};
+    this.doc.submitOp(op, {source: this});
 };
 
-StringBinding.prototype._remove = function(index, text) {
-  var path = this.path.concat(index);
-  var op = {p: path, sd: text};
-  this.doc.submitOp(op, {source: this});
+ShareDBCodeMirror.prototype._remove = function(index, text) {
+    var path = this.path.concat(index);
+    var op = {p: path, sd: text};
+    this.doc.submitOp(op, {source: this});
 };
 
 function isSubpath(path, testPath) {
-  for (var i = 0; i < path.length; i++) {
-    if (testPath[i] !== path[i]) return false;
-  }
-  return true;
+    for (var i = 0; i < path.length; i++) {
+        if (testPath[i] !== path[i]) return false;
+    }
+    return true;
 }
